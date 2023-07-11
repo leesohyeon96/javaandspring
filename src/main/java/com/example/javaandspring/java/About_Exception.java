34282df2480e525024485343eb26b97e9dae7866
vkfks <vkfkswjs26@gmail.com>
2023-07-11 22:17:04 +0900

    java and spring - Exception 종류 3가지

---
+++ src/main/java/com/example/javaandspring/java/About_Exception.java
@@ -0,0 +1,16 @@
+package com.example.javaandspring.java;
+
+public class About_Exception {
+
+    // *자바의 예외
+    // 종류
+    // 1. UnCheckedException(=RuntimeException)
+    // 2. CheckedException
+        // - RuntimeException이외의 모든 Exception의 하위클래스들
+        // -
+    // 3. Error(에러)
+        // - 시스템이 비정상적인 상황이 발생할 경우 발생
+        // - ex) 메모리 부족, 스택오버플로우
+        // - 에러는 개발자가 예측하기 어렵고, 처리할 수 있는 방법x
+
+}
